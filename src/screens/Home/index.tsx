e0b43b3e8631f92eddc0bf702963eacabdd24768
jaquelinepires <piresjaqueline17@gmail.com>
2022-02-17 16:21:32 -0300

    Home Header creation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
-
+import { StatusBar } from 'react-native';
+import Logo from '../../assets/logo.svg'
+import { RFValue } from 'react-native-responsive-fontsize';
 import {
   Container,
-  Title
+  Header,
+  TotalCars,
+  HeaderContent
 } from './styles';
+
 
 export function Home(){
   return (
    <Container>
-    <Title>Home</Title>
+     <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent 
+     />
+    <Header>
+      <HeaderContent>
+        <Logo 
+          width={RFValue(100)}
+          height={RFValue(12)}
+        />
+        <TotalCars>
+          Total 12 carros
+        </TotalCars>
+      </HeaderContent>
+    </Header>
    </Container>
   );
 }
